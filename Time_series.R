f745db4806cd325fa0168ab323b85825f98d8944
rlrichards <robbielrichards@gmail.com>
2022-02-28 17:55:22 -0500

    png of time series for readme

--- Time_series.R
+++ Time_series.R
@@ -10,7 +10,7 @@ predictions <- read.csv("cherry_predictions.csv") %>%
   dplyr::mutate(type = "Predicted") 
 
 kyoto <- read.csv("data/kyoto.csv") %>% 
-  dplyr::select(year, location, bloom_doy) #%>% 
+  dplyr::select(year, location, bloom_doy) %>% 
   dplyr::filter(year > 1890) # Reduce dataset to comparable years for visualization
 
 liestal <- read.csv("data/liestal.csv") %>% 
@@ -31,6 +31,26 @@ names(locations_labs) <- c("kyoto", "liestal", "vancouver", "washingtondc")
 
 # Plot the time series of past and predicted data for each location
 pdf("figures/time_seriesPlot.pdf")
+ggplot(data = all_doys, aes(x = year, y = bloom_doy, color = type))+
+  geom_line(size = 1)+
+  facet_wrap(~location, labeller = labeller(location = locations_labs))+
+  theme(panel.background = element_blank(),
+        panel.border = element_rect(color = "black", fill = NA),
+        strip.background = element_rect(fill = "lavenderblush1"),
+        strip.text = element_text(color = "black", size = 12),
+        axis.title = element_text(color = "black", size = 16, face = "bold"),
+        axis.text = element_text(color = "black", size = 12),
+        legend.key = element_blank(),
+        legend.background = element_blank(),
+        legend.title = element_text(color = "black", face = "bold"),
+        legend.position = c(0.09,0.4))+
+  scale_color_manual(values = c("gray", "hotpink"))+
+  scale_x_continuous(breaks = c(1900,1925,1950,1975,2000,2021), limits = c(1900, 2032))+
+  labs(x = "Year", y = "Peak bloom (days since Jan 1st)", color = "Data Type")
+dev.off()
+
+
+png("figures/time_seriesPlot.png")
 ggplot(data = all_doys, aes(x = year, y = bloom_doy, color = type))+
   geom_line(size = 1)+
   facet_wrap(~location, labeller = labeller(location = locations_labs))+
